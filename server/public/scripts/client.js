30268163ea1bed128e504858903c6083f48d0a27
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-01 11:04:52 -0600

    Adding put functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -13,11 +13,12 @@ $(document).ready(function(){
           var currentBook = response[i]; // Loops through books - This is an object
           var $newBook = $('<tr>'); // Creating a new row for each book
           $newBook.data('id', currentBook.id);
-          $newBook.append('<td>' + currentBook.title + '</td>');
+          $newBook.append('<td><input value="' + currentBook.title + '" class="bookTitle"></td>');
           $newBook.append('<td>' + currentBook.author + '</td>');
           $newBook.append('<td>' + currentBook.edition + '</td>');
           $newBook.append('<td>' + currentBook.publisher + '</td>');
           $newBook.append('<td><button class="deleteButton">Delete</button></td>');
+          $newBook.append('<td><button class="saveButton">Save</button></td>');
           $('#bookShelf').prepend($newBook);
         }
       }
@@ -28,7 +29,6 @@ $(document).ready(function(){
     event.preventDefault();
     var newBookObject = {};
     var formFields = $(this).serializeArray();
-
     formFields.forEach(function (field) {
       newBookObject[field.name] = field.value;
     });
@@ -52,6 +52,24 @@ $(document).ready(function(){
     $.ajax({
       type: 'DELETE',
       url: '/books/delete/' + idOfBookToDelete,
+      success: function(response) {
+        console.log(response);
+        getBookData();
+      }
+    })
+  });
+
+  $('#bookShelf').on('click', '.saveButton', function(){
+    var idOfBookToSave = $(this).parent().parent().data().id;
+    var titleOfBookToSave = $(this).parent().parent().find('.bookTitle').val();
+    var bookObjectToSave = {
+      title: titleOfBookToSave
+    };
+    // for waldo, number 48 -> /books/save/48
+    $.ajax({
+      type: 'PUT',
+      url: '/books/save/' + idOfBookToSave,
+      data: bookObjectToSave,
       success: function(response) {
         console.log(response);
         getBookData();
--- server/routes/books.js
+++ server/routes/books.js
@@ -49,16 +49,16 @@ router.post('/new', function(req, res){
       // We connected to the database!!!
       // Now, we're gonna' git stuff!!!!!
       client.query('INSERT INTO books (title, author, edition, publisher) VALUES ($1, $2, $3, $4);',
-        [newBook.title, newBook.author, newBook.edition, newBook.publisher],
-        function(errorMakingQuery, result){
-          done();
-          if(errorMakingQuery) {
-            console.log('Error making the database query: ', errorMakingQuery);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(201);
-          }
-        });
+      [newBook.title, newBook.author, newBook.edition, newBook.publisher],
+      function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
     }
   });
 });
@@ -78,19 +78,50 @@ router.delete('/delete/:id', function(req, res){
       // We connected to the database!!!
       // Now, we're gonna' delete stuff!!!!!
       client.query('DELETE FROM books WHERE id=$1;', // This is the SQL query
-        [bookId], // This is the array of things that replaces the $1, $2, $3 in the query
-        function(errorMakingQuery, result){ // This is the function that runs after the query takes place
-          done();
-          if(errorMakingQuery) {
-            console.log('Error making the database query: ', errorMakingQuery);
-            res.sendStatus(500);
-          } else {
-            res.sendStatus(200);
-          }
-        });
+      [bookId], // This is the array of things that replaces the $1, $2, $3 in the query
+      function(errorMakingQuery, result){ // This is the function that runs after the query takes place
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(202);
+        }
+      });
     }
   });
-});
+}); // closing delete request
+
+
+// for update -> /save/48
+router.put('/save/:id', function(req, res){
+  var bookId = req.params.id;
+  var bookObject = req.body;
+  // UPDATE books SET title='The Yodler' WHERE id=40;
+  console.log('book of id to save: ', bookId);
+  // Connecting to, and deleting row from the database
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      // There was an error connecting to the database
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      // We connected to the database!!!
+      // Now, we're gonna' update stuff!!!!!
+      client.query('UPDATE books SET title=$1 WHERE id=$2;', // This is the SQL query
+      [bookObject.title, bookId], // This is the array of things that replaces the $1, $2, $3 in the query
+      function(errorMakingQuery, result){ // This is the function that runs after the query takes place
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(202);
+        }
+      });
+    }
+  });
+}); // closing put request
 
 
 module.exports = router;
