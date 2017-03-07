e6ebcaf1e5fac063da93556955dc2a3896ec4195
Paul Dunkirk <pauldunkirk@gmail.com>
2017-03-07 14:29:43 -0600

    post works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,26 +1,36 @@
 var myApp = angular.module('BooksApp', []);
 myApp.controller('BooksController', ['$http', function($http) {
-  console.log('BooksController sourced');
-  var self = this;
-  self.booksList;
+    // console.log('BooksController sourced');
+    var self = this;
+    self.booksList;
+    self.newBookObject = {};
+    getBooks();
+    function getBooks() {
+        $http({
+            method: 'GET',
+            url: '/books'
+        }).then(function(response) {
+            // console.log(response.data);
+            self.booksList = response.data;
+        });
+    }
+    self.newBook = function() {
+        $http({
+            method: 'POST',
+            url: '/books/new',
+            data: self.newBookObject
+        }).then(function(response) { // end http
+            console.log(response);
+            getBooks();
+        }); // end .then function
+    };// end newBook function
 
-getBooks();
-function getBooks(){
-  $http({
-    method: 'GET',
-    url: '/books'
-  }).then(function(response){
-    console.log(response.data);
-    self.booksList = response.data;
-  });
-}
+    // success: function(response){
+    //   console.log(response);
+    //   getBookData();
+    //   $('#newBookForm > input').val('');
 
-
-
-// self.booksList.push(angular.copy(self.booksList));
-
-
-
+    // self.booksList.push(angular.copy(self.booksList));
 
 
 }]);
