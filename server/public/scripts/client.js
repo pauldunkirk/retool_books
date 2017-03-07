5a68a7350d4bc6f606fcb3f48220e690b712acb5
Paul Dunkirk <pauldunkirk@gmail.com>
2017-03-07 16:52:16 -0600

    almost got er done working on put as last step

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,10 @@ var myApp = angular.module('BooksApp', []);
 myApp.controller('BooksController', ['$http', function($http) {
     // console.log('BooksController sourced');
     var self = this;
-    self.booksList;
     self.newBookObject = {};
+// self.booksList.id
     getBooks();
+
     function getBooks() {
         $http({
             method: 'GET',
@@ -25,12 +26,56 @@ myApp.controller('BooksController', ['$http', function($http) {
         }); // end .then function
     };// end newBook function
 
-    // success: function(response){
-    //   console.log(response);
-    //   getBookData();
-    //   $('#newBookForm > input').val('');
 
-    // self.booksList.push(angular.copy(self.booksList));
+      self.deleteBook = function(bookIdToDelete) {
+        console.log(bookIdToDelete);
+      $http({
+          method: 'DELETE',
+          url: '/books/delete/' + bookIdToDelete
+      }).then(function(response) {
+          console.log(response);
+          getBooks();
+      });
+    };
+
+    self.deleteBook = function(bookIdToDelete) {
+      console.log(bookIdToDelete);
+    $http({
+        method: 'PUT',
+        url: '/books/delete/' + bookIdToDelete
+    }).then(function(response) {
+        console.log(response);
+        getBooks();
+    });
+    };
+
+
+    // $('#bookShelf').on('click', '.saveButton', function(){
+    //   var idOfBookToSave = $(this).parent().parent().data().id;
+    //   var titleOfBookToSave = $(this).parent().parent().find('.bookTitle').val();
+    //   var bookObjectToSave = {
+    //     title: titleOfBookToSave
+    //   };
+    //   // for waldo, number 48 -> /books/save/48
+    //   $.ajax({
+    //     type: 'PUT',
+    //     url: '/books/save/' + idOfBookToSave,
+    //     data: bookObjectToSave,
+    //     success: function(response) {
+    //       console.log(response);
+    //       getBookData();
+    //     }
+    //   })
+    // });
+
+
+
+
+
+
+
+
+
 
 
 }]);
